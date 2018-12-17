ada3dd3f6d371bbed5e71bf03f6c309b7fa5c289
ElQwerto110100100 <joshyboy111@hotmail.com.au>
2018-12-17 16:01:50 +1100

    finished beginere tut

--- Python/Djaingo/mysite/polls/admin.py
+++ Python/Djaingo/mysite/polls/admin.py
@@ -16,6 +16,8 @@ class QuestionAdmin(admin.ModelAdmin):
         ('Date infomation', {'fields': ['pub_date'], 'classes':['collapse']}),
     ]
     inlines = [ChoiceInline]
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
     list_display = ('question_text', 'pub_date', 'was_published_recently')
 
 admin.site.register(Question, QuestionAdmin)

15576b3e5f386bbfcb4ac3feb37516a704c8cfb8
ElQwerto110100100 <joshyboy111@hotmail.com.au>
2018-12-17 15:04:22 +1100

    learning dijango up to part7

--- Python/Djaingo/mysite/polls/admin.py
+++ Python/Djaingo/mysite/polls/admin.py
@@ -4,6 +4,18 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
-from .models import Question
+from .models import Choice, Question
 
-admin.site.register(Question)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields':['question_text']}),
+        ('Date infomation', {'fields': ['pub_date'], 'classes':['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+
+admin.site.register(Question, QuestionAdmin)

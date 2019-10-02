9d0a8ae125dad2586cd60e1a36f2a3c6bebbbc65
ElQwerto110100100 <joshyboy111@hotmail.com.au>
2019-10-02 16:39:04 +1000

    started c++

---
+++ C++/helloWorld.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string greeting = "Hello";
+
+int main() {
+  cout << "Hello World!";
+  cout << "Hello World!\n";
+  cout << greeting;
+  return 0;
+}

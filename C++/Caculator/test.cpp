da0941992b615ac92f4ab876cd55490121bb8218
ElQwerto110100100 <joshyboy111@hotmail.com.au>
2019-10-03 17:51:30 +1000

    testing so far not working

--- C++/Caculator/test.cpp
+++ C++/Caculator/test.cpp
@@ -1,5 +1,6 @@
 #include <windows.h>
-#include "resource\resource.h"
+#define ID_FILE_EXIT 9001
+#define ID_STUFF_GO 9002
 
 /*int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
     char* lpCmdLine, int nCmdShow)
@@ -15,6 +16,48 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
     switch(msg)
     {
+      case WM_CREATE:
+        {
+           HMENU hMenu, hSubMenu;
+           HICON hIcon, hIconSm;
+
+           hMenu = CreateMenu();
+
+           hSubMenu = CreatePopupMenu();
+           AppendMenu(hSubMenu, MF_STRING, ID_FILE_EXIT, "E&xit");
+           AppendMenu(hMenu, MF_STRING | MF_POPUP, (UINT)hSubMenu, "&File");
+
+           hSubMenu = CreatePopupMenu();
+           AppendMenu(hSubMenu, MF_STRING, ID_STUFF_GO, "&Go");
+           AppendMenu(hMenu, MF_STRING | MF_POPUP, (UINT)hSubMenu, "&Stuff");
+
+           SetMenu(hwnd, hMenu);
+
+
+           hIcon = LoadImage(NULL, "my_icon.ico", IMAGE_ICON, 32, 32, LR_LOADFROMFILE);
+           if(hIcon)
+               SendMessage(hwnd, WM_SETICON, ICON_BIG, (LPARAM)hIcon);
+           else
+               MessageBox(hwnd, "Could not load large icon!", "Error", MB_OK | MB_ICONERROR);
+
+           hIconSm = LoadImage(NULL, "my_icon.ico", IMAGE_ICON, 16, 16, LR_LOADFROMFILE);
+           if(hIconSm)
+               SendMessage(hwnd, WM_SETICON, ICON_SMALL, (LPARAM)hIconSm);
+           else
+               MessageBox(hwnd, "Could not load small icon!", "Error", MB_OK | MB_ICONERROR);
+        }
+        break;
+        case WM_COMMAND:
+            switch(LOWORD(wParam))
+            {
+                case ID_FILE_EXIT:
+                    PostMessage(hwnd, WM_CLOSE, 0, 0);
+                break;
+                case ID_STUFF_GO:
+
+                break;
+            }
+        break;
         case WM_LBUTTONDOWN:
         {
           char szFileName[MAX_PATH];
@@ -51,17 +94,18 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
     wc.cbClsExtra    = 0;
     wc.cbWndExtra    = 0;
     wc.hInstance     = hInstance;
-    //wc.hIcon         = LoadIcon(NULL, IDI_APPLICATION);
+    wc.hIcon         = LoadIcon(NULL, IDI_APPLICATION);
     wc.hCursor       = LoadCursor(NULL, IDC_ARROW);
     wc.hbrBackground = (HBRUSH)(COLOR_WINDOW+1);
-    //wc.lpszMenuName  = NULL;
+    wc.lpszMenuName  = NULL;
     wc.lpszClassName = g_szClassName;
-    //wc.hIconSm       = LoadIcon(NULL, IDI_APPLICATION);
+    wc.hIconSm       = LoadIcon(NULL, IDI_APPLICATION);
 
-    //new icon and menu 1.
+    /*//new icon and menu 1.
     wc.lpszMenuName  = MAKEINTRESOURCE(IDR_MYMENU);
     wc.hIcon  = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_MYICON));
     wc.hIconSm  = (HICON)LoadImage(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_MYICON), IMAGE_ICON, 16, 16, 0);
+    */
 
     if(!RegisterClassEx(&wc))
     {

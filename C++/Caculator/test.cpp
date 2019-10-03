4c6692024a6853d04369d6d134b3ce9da302f662
ElQwerto110100100 <joshyboy111@hotmail.com.au>
2019-10-03 17:28:08 +1000

    testing with icons and menus with header file and rc files

--- C++/Caculator/test.cpp
+++ C++/Caculator/test.cpp
@@ -1,4 +1,5 @@
 #include <windows.h>
+#include "resource\resource.h"
 
 /*int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
     char* lpCmdLine, int nCmdShow)
@@ -14,6 +15,15 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
     switch(msg)
     {
+        case WM_LBUTTONDOWN:
+        {
+          char szFileName[MAX_PATH];
+          HINSTANCE hInstance = GetModuleHandle(NULL);
+          GetModuleFileName(hInstance, szFileName, MAX_PATH);
+          MessageBox(hwnd, szFileName, "This program is:", MB_OK | MB_ICONINFORMATION);
+          //PostMessage(hwnd,WM_CLOSE,0,0);
+        }
+        break;
         case WM_CLOSE:
             DestroyWindow(hwnd);
         break;
@@ -29,6 +39,7 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
     LPSTR lpCmdLine, int nCmdShow)
 {
+
     WNDCLASSEX wc;
     HWND hwnd;
     MSG Msg;
@@ -40,12 +51,17 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
     wc.cbClsExtra    = 0;
     wc.cbWndExtra    = 0;
     wc.hInstance     = hInstance;
-    wc.hIcon         = LoadIcon(NULL, IDI_APPLICATION);
+    //wc.hIcon         = LoadIcon(NULL, IDI_APPLICATION);
     wc.hCursor       = LoadCursor(NULL, IDC_ARROW);
     wc.hbrBackground = (HBRUSH)(COLOR_WINDOW+1);
-    wc.lpszMenuName  = NULL;
+    //wc.lpszMenuName  = NULL;
     wc.lpszClassName = g_szClassName;
-    wc.hIconSm       = LoadIcon(NULL, IDI_APPLICATION);
+    //wc.hIconSm       = LoadIcon(NULL, IDI_APPLICATION);
+
+    //new icon and menu 1.
+    wc.lpszMenuName  = MAKEINTRESOURCE(IDR_MYMENU);
+    wc.hIcon  = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_MYICON));
+    wc.hIconSm  = (HICON)LoadImage(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_MYICON), IMAGE_ICON, 16, 16, 0);
 
     if(!RegisterClassEx(&wc))
     {
@@ -81,3 +97,8 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
     }
     return Msg.wParam;
 }
+
+/*
+To link the resource file to excuteable. use windres [the resource file.rc] -O coff -o [the resource file.rs]
+then compile with g++ [source.cpp] [the resourcefile.rs] -o [excutable name.exe]
+*/

749bf8fea9da3a07af1bc50910bc9dbf11633f15
ElQwerto110100100 <joshyboy111@hotmail.com.au>
2020-05-17 21:44:16 +1000

    part 4

--- Web/angular/test/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ Web/angular/test/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,15 +10,24 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
 
   selectedHero: Hero;
+
+  heroes: Hero[];
+
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
-  }
-  constructor() { }
-
-  ngOnInit(): void {
+    this.messageService.add(`HeroService: Selected hero name=${hero.name} id=${hero.id}`);
   }
 
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
 }
